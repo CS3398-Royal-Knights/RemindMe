0e1c2dc666a28e6ed305faa5a20b86ded511cecb
Taurino <t_t23@txstate.edu>
2016-10-10 18:16:18 -0500

    complete code for splash screen and icon

---
+++ app/src/main/java/com/cs3398royal/remindme/remindme/Splash.java
@@ -0,0 +1,47 @@
+package com.cs3398royal.remindme.remindme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by ttostado on 10/8/2016.
+ */
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+
+        iv.startAnimation(an);
+
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
