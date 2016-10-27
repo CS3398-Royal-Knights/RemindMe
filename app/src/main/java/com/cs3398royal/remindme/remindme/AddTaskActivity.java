1d659b89f12fd63ab81126fa7a33a38f112dedbf
Brandon <bjc116@txstate.edu>
2016-10-26 23:56:14 -0500

    Added the ability to create a Date chooser when the dateText Edit Text is selected.

--- app/src/main/java/com/cs3398royal/remindme/remindme/AddTaskActivity.java
+++ app/src/main/java/com/cs3398royal/remindme/remindme/AddTaskActivity.java
@@ -1,19 +1,29 @@
 package com.cs3398royal.remindme.remindme;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddTaskActivity extends AppCompatActivity {
 
     private RelativeLayout layout;
-
+    private EditText dateText;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +32,30 @@ public class AddTaskActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         layout = (RelativeLayout) findViewById(R.id.content_add_task);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        dateText = (EditText) findViewById(R.id.editText2);
+        dateText.setInputType(InputType.TYPE_NULL);
+        Calendar newCalendar = Calendar.getInstance();
+        dateFormatter = new SimpleDateFormat("E, MMM dd, yyyy", Locale.US);
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dateText.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,4 +78,6 @@ public class AddTaskActivity extends AppCompatActivity {
         }
         return false;
     }
+
+
 }
